b010fc358a7f40be8479e4d8d46c75f4058b580d
Jennp1987 <44036108+Jennp1987@users.noreply.github.com>
2019-02-14 17:41:09 -0800

    assignment1

---
+++ js/script.js
@@ -0,0 +1,237 @@
+/*eslint-env browser*/
+
+//STEP 1
+/*
+var some_Month;
+function theMonth();
+var current_Month;
+var summer_Month;
+var myLibrary_function
+*/
+
+//Step 2
+/*
+45
+"This is a string literal"
+true
+null
+*/
+
+//Step 3
+/*
+var localishRoadTrip = "High Sierras";
+var crossCountryRoadTrip = "North to the Cascade Range";
+*/
+
+//*Step 4
+/*
+var sfirstName;
+var slastName;
+var s_address;
+var s_city;
+var s_state;
+var szipCode;
+var syourAge;
+var sreferralSource;
+var smayWeContactYou;
+*/
+
+//Step 5
+/*
+var sfirstName;
+var slastName;
+var s_address;
+sfirstName = "Jennifer";
+slastName = "Phillips";
+s_address = "4460 Parks Ave";
+
+var sfirstName = "Jennifer";
+var slastName = "Phillips";
+var s_address = "4460 Parks Ave";
+
+var sfirstName = "Jennifer", slastName = "Phillips", s_address = "4460 Parks Ave";
+*/
+
+//Step 6
+/*
+var dogs_Age = 77;
+window.console.log(typeof dogs_Age);
+window.console.log("The dog's age is:" + dogs_Age + "years.");
+*/
+
+//Step 7
+/*
+var nothingIsGuaranteed = true;
+window.console.log(typeof nothingIsGuaranteed);
+window.console.log("Nothing is guaranteed" + " " + "-" + " " + nothingIsGuaranteed);
+*/
+
+/*
+var myAge = 31;
+window.console.log(typeof myAge);
+window.console.log(myAge + " " + "true");
+*/
+
+//Step 8
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+*/
+
+//Step 9
+/*
+var x = null;
+window.console.log(x);
+*/
+/*
+var x;
+window.console.log(x);
+*/
+
+//Step 10
+/*
+window.console.log(typeof "walrus");
+window.console.log(typeof 31);
+window.console.log(typeof true);
+window.console.log(typeof x);
+window.console.log(typeof[1,2,3]);
+*/
+
+//Step 11
+/*
+var myClass = ("Hello" + " " + "Zak Ruvacalba" + ", welcome to the JavaScript class!");
+window.alert(myClass);
+*/
+
+//Step12
+/*
+var name = "Jenn Phillips";
+var myClass = ("Hello" + " " + name + ", welcome to the JavaScript class!");
+window.alert(myClass);
+*/
+
+//Step13
+/*
+var course = "JavaScript";
+var name = "Jenn Phillips";
+var myClass = ("Hello" + " " + name + ", welcome to the" + " " +  course + " " + "class!");
+window.alert(myClass);
+*/
+
+//Step 14
+/*
+var course = "JavaScript";
+var name = "Jenn Phillips";
+var myClass = ("Hello" + " " + name + ".\n" + "Welcome to the" + " " + course + " " + "class!");
+window.alert(myClass);
+*/
+
+//Step 15
+/*
+var course = "JavaScript";
+var name = "what is your name?";
+var yourClass = ("Hello" + " " + name + " .\n" + "Welcome to the" + " " + course + " " + "class!");
+window.prompt(yourClass);
+*/
+
+//Step 16
+/*
+var course = "What is the class you are taking?";
+var name = "What is your name?";
+var yourClass = (" Hello." + " " + name + "\n " + course );
+window.prompt(yourClass);
+*/
+
+//Step 17
+/*
+var x = 10;
+var y = 20;
+window.console.log( x + y);
+*/
+
+//Step 18
+/*
+var x = 20;
+x = x += 20;
+window.console.log(x);
+*/
+
+//Step 19
+/*
+var x = 20;
+x = x *= 5;
+window.console.log(x);
+*/
+
+//Step 20
+/*
+var x = 20 % 3;
+x = x /= 1;
+window.console.log(x);
+*/
+
+//step 21
+/*
+var roadTrip1 = "Baja";
+var roadTrip2 = "Joshua Tree";
+window.console.log(roadTrip1 === "Baja" && roadTrip2 === "Joshua Tree");
+*/
+
+//step 22
+/*
+var x = 1;
+var y = 3;
+window.console.log(x > y || y >= 10);
+*/
+
+//Step 23
+/*
+var widget = new Object(); 
+window.console.log(typeof widget);
+*/
+
+
+//Step 24
+/*
+var widget = new Object();
+window.console.log (widget instanceof Object);
+*/
+
+//Step 25
+/*
+var widget = new Object();
+window.console.log(widget instanceof String);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
